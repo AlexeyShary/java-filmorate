7e5edc2e92055158c60f50357b336c5e726da4b5
BadIrbis <badirbis@yandex.ru>
2023-05-07 21:48:17 +0300

    fix: Убрал лишнюю аннотацию User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import ru.yandex.practicum.filmorate.validation.NoSpaces;
 
 import javax.validation.constraints.Email;
@@ -12,7 +11,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@EqualsAndHashCode
 public class User {
     private long id;
     @Email(message = "E-mail должен быть корректным.")
