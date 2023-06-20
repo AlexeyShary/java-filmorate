c3a755f01080fbd837acfae87d6a1a02f6d0b671
Olesia Rumiantseva <pymanceva@yandex.ru>
2023-06-21 02:14:33 +0300

    feat: добавить вывод фильмов режиссера с сортировкой

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -83,4 +83,8 @@ public class FilmService {
 
         log.debug("Удален лайк фильму {} от пользователя {}", id, userId);
     }
+
+    public Collection<Film> getDirectorFilmsSorted(long directorId, String sortBy) {
+        return filmStorage.getDirectorFilmsSorted(directorId, sortBy);
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -14,4 +14,6 @@ public interface FilmStorage {
     void delete(long id);
 
     Film update(Film film);
+
+    Collection<Film> getDirectorFilmsSorted(long directorId, String sortBy);
 }
